a80c9880c9bd5c34e30e3384c6ad6a32c564fa07
Alexey Demin <mskdemin@gmail.com>
2023-09-09 21:47:59 +0300

    about delete required

--- middlwares/validators/validators.js
+++ middlwares/validators/validators.js
@@ -18,7 +18,7 @@ const validateSingIn = celebrate({
 const validateProfileUpdate = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    about: Joi.string().min(2).max(30).required(),
+    // about: Joi.string().min(2).max(30).required(),
   }),
 });
 
