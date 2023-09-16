0d5d5881880fecb4150518f49c84cb4e5940de77
Alexey Demin <mskdemin@gmail.com>
2023-09-16 12:52:00 +0300

    Поправил редактирование профиля добавив конфликт

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,19 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const { JWT } = require('../utils/config');
-const NotFoundError = require('../errors/NotFoundError');
-const ConflictError = require('../errors/ConflictError');
-const ForbiddenError = require('../errors/ForbiddenError');
-const UnauthorizedError = require('../errors/UnauthorizedError');
-const BadRequestError = require('../errors/BadRequestError');
-const InternalServerError = require('../errors/InternalServerError');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const { JWT } = require("../utils/config");
+const NotFoundError = require("../errors/NotFoundError");
+const ConflictError = require("../errors/ConflictError");
+const ForbiddenError = require("../errors/ForbiddenError");
+const UnauthorizedError = require("../errors/UnauthorizedError");
+const BadRequestError = require("../errors/BadRequestError");
+const InternalServerError = require("../errors/InternalServerError");
 
 const createUser = (req, res, next) => {
-  const {
-    name,
-    email,
-    password,
-  } = req.body;
+  const { name, email, password } = req.body;
 
-  bcrypt.hash(String(password), 10)
+  bcrypt
+    .hash(String(password), 10)
     .then((hashedPassword) => {
       User.create({
         name,
@@ -28,7 +25,11 @@ const createUser = (req, res, next) => {
         })
         .catch((err) => {
           if (err.code === 11000) {
-            next(new ConflictError({ message: 'Такой пользователь уже зарегистрирован.' }));
+            next(
+              new ConflictError({
+                message: "Такой пользователь уже зарегистрирован.",
+              })
+            );
           } else {
             next(err);
           }
@@ -47,32 +48,34 @@ const login = (req, res, next) => {
   const { email, password } = req.body;
   // для предотвращения лишних пустых запросов
   if (!email || !password) {
-    next(new ForbiddenError({ message: 'email или пароль не введены.' }));
+    next(new ForbiddenError({ message: "email или пароль не введены." }));
     return;
   }
 
   User.findOne({ email })
-    .select('+password')
+    .select("+password")
     .then((user) => {
-      bcrypt.compare(String(password), user.password)
-        .then((isValidUser) => {
-          if (isValidUser) {
-            const token = jwt.sign(
-              { _id: user._id },
-              JWT,
-            );
-            res.send({ token });
-          } else {
-            next(new UnauthorizedError({ message: 'Учетные данные введены неверно.' }));
-          }
-        });
+      bcrypt.compare(String(password), user.password).then((isValidUser) => {
+        if (isValidUser) {
+          const token = jwt.sign({ _id: user._id }, JWT);
+          res.send({ token });
+        } else {
+          next(
+            new UnauthorizedError({
+              message: "Учетные данные введены неверно.",
+            })
+          );
+        }
+      });
     })
     .catch(next);
 };
 
 const getUsersById = (req, res, next) => {
   User.findById(req.params.id || req.user._id)
-    .orFail(() => next(new NotFoundError({ message: 'Пользователь с таким id не найден.' })))
+    .orFail(() =>
+      next(new NotFoundError({ message: "Пользователь с таким id не найден." }))
+    )
     .then((user) => {
       res.send(user);
     })
@@ -86,12 +89,23 @@ const updateProfile = (req, res, next) => {
     new: true,
     runValidators: true,
   })
-    .then((user) => res.status(200).send(user))
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError("Искали, но не нашли");
+      }
+      res.send(user);
+    })
     .catch((err) => {
-      if (err.name === 'CastError' || err.name === 'ValidationError') {
-        next(new BadRequestError({ message: 'Что то не так.' }));
+      if (err.code === 11000) {
+        next(new ConflictError("Такой email занят"));
+        return;
+      }
+      if (err.name === "CastError" || err.name === "ValidationError") {
+        next(new BadRequestError({ message: "Что то не так." }));
       } else {
-        next(new InternalServerError({ message: 'Ошибка на стороне сервера.' }));
+        next(
+          new InternalServerError({ message: "Ошибка на стороне сервера." })
+        );
       }
     })
     .catch(next);
